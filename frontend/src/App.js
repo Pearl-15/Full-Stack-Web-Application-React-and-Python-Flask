a24e32c676d72e0b729dbe6da7640218e7d6fa8b
Pearl <pearlkhinkhin.15@gmail.com>
2023-04-08 16:07:19 +0800

    added Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
+      <Header title="Images Gallery"/>
      <h1>Images Gallery</h1>
      <p> This is the additional changes </p>
     </div>
