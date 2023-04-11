69ff124440610564983722bc6208fc5a4b3abe33
Pearl <pearlkhinkhin.15@gmail.com>
2023-04-11 22:42:20 +0800

    initial api and python virtual env

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,20 +1,57 @@
-import React from "react";
-import { Card, Button } from "react-bootstrap";
+import React, {useState} from "react";
+import { Card, Button, CardGroup } from "react-bootstrap";
 
 const ImageCard = ({image, deleteImage}) => {
+
+  const [showFullText, setShowFullText] = useState(false);
+
+  const toggleShowFullText = () =>{
+    setShowFullText(!showFullText);
+  };
+
+  const truncateText = (text, maxLength) => {
+    if(text.length <= maxLength){
+      return text;
+    }
+    return text.slice(0,maxLength)+"...";
+  }; 
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={image.urls.small}/>
-      <Card.Body>
-        <Card.Title>{image.title}</Card.Title>
-        <Card.Text>
-          {image.description || image.alt_description}
-        </Card.Text>
-        <Button variant="primary" onClick={() => {
-            deleteImage(image.id)
-        }}>Delete</Button>
-      </Card.Body>
-    </Card>
+    <CardGroup>
+      <Card style={{ width: '450px', height: '300px', position: 'relative' }} >
+        <Card.Body className="d-flex flex-column justify-content-end align-items-center">
+        <Card.Img variant="top" src={image.urls.small} style={{ objectFit:'cover', width:'100%', maxHeight: '200px',minHeight:'200px', overflow: 'hidden' }} />
+          <Card.Title>{image.title}</Card.Title>
+          <Card.Text style={{ maxHeight: '150px',minHeight:'150px',overflow: 'hidden' }}>
+            {showFullText
+              ? image.description || image.alt_description
+              : truncateText(image.description || image.alt_description, 30)}
+            {!showFullText && (
+              <Button
+                variant="link"
+                onClick={toggleShowFullText}
+                style={{ paddingLeft: 0, marginLeft: 0,fontSize:'0.8rem' }}
+              >
+                See More
+              </Button>
+            )}
+            {showFullText && (
+            <Button
+              variant="link"
+              onClick={toggleShowFullText}
+              style={{ paddingLeft: 0, marginLeft: 0 , fontSize:'0.8rem'}}
+            >
+              Show Less
+            </Button>
+          )}
+            
+          </Card.Text>
+          <Button style={{ position: 'absolute', bottom: '10px', right: '10px' ,backgroundColor: "#f28a29" ,borderStyle: "none"}} className="btn-sm" onClick={() => {
+              deleteImage(image.id)
+          }}>Delete</Button>
+        </Card.Body>
+      </Card>
+    </CardGroup>
   );
 };
 export default ImageCard;
